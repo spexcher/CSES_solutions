bcd2407a701e1c17d5911515337ab844c2786c65
Gourab Modak <137685981+spexcher@users.noreply.github.com>
2026-07-15 03:33:22 +0530

    Add C++ solution for Counting_Numbers

---
+++ Dynamic_Programming/Counting_Numbers/Counting_Numbers.cpp
@@ -0,0 +1,10 @@
+/*
+ * Problem Name: Counting_Numbers
+ * Language: C++
+ * Category: Dynamic_Programming
+ * Date: 2026-07-14
+ */
+
+input/code.cpp:15: warning: ignoring '#pragma GCC optimization' [-Wunknown-pragmas]
+   15 | #pragma GCC optimization("O3")
+      |
